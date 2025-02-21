7f4585560006cd38cf94c2cfe156fd279570c32f
Phatjira Rungsakullikit <p.rungsakullikit1@hm.edu>
2025-02-21 10:38:24 +0100

    add is_know function and update loading data

--- main.py
+++ main.py
@@ -2,32 +2,65 @@ from tkinter import *
 import pandas
 import random
 
+from debugpy.common.timestamp import current
+
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
+current_card = {} # Store the current card
+to_learn = {}
+
+# Load Data
+try:
+    data = pandas.read_csv("data/words_to_learn.csv") # Load remaining words
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+    original_data.to_csv("data/words_to_learn.csv", index=False) # Save for future use
+else:
+    to_learn = data.to_dict(orient="records") # Converts to the list of dictionary
+
+def next_card():
+    """Show a random card."""
+    global current_card # This statement is necessary because we need the same word to be accessible in both the next_card() and flip_card() functions
+    global flip_timer
+
+    # Cancel any previously scheduled flip to avoid overlapping timers
+    window.after_cancel(flip_timer)
+
+    current_card = random.choice(to_learn)  # Pick a random word
+
+    # Reset to front image
+    canvas.itemconfig(canvas_image, image=card_front)
+    canvas.itemconfig(card_title, text="French", fill="black")  # Update title text
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")  # Update word text
+
+    # Schedule the card to flip after 3 secs
+    flip_timer = window.after(3000, flip_card)
+
+def flip_card():
+    """Flip the card to show the English translation."""
+    canvas.itemconfig(canvas_image, image=card_back) # Change to card back image
+    canvas.itemconfig(card_title, text="English", fill="white")  # Change title & color
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")  # Change word & color
+
+def is_known():
+    """Remove the current word from the list and update the CSV file."""
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 # Window Setup
 window = Tk()
 window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
-
-# Load Data
-data = pandas.read_csv("data/french_words.csv") # data is now a DataFrame containing CSV data
-# Converts the DataFrame into a list of dictionaries
-data_dict = data.to_dict(orient="records")
-print(data_dict)
-
-# Function to Show a Random Card
-def next_card():
-    global current_card
-    current_card = random.choice(data_dict)  # Pick a random word
-    canvas.itemconfig(card_title, text="French")  # Update title text
-    canvas.itemconfig(card_word, text=current_card["French"])  # Update word text
-
 # Create Canvas
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+card_back = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front)
 
 # Text on Canvas
 card_title = canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
@@ -41,12 +74,14 @@ wrong_button = Button(image=wrong, highlightthickness=0, command=next_card)
 wrong_button.grid(column=1, row=1)
 
 right = PhotoImage(file="images/right.png")
-right_button = Button(image=right, highlightthickness=0, command=next_card)
+right_button = Button(image=right, highlightthickness=0, command=is_known)
 right_button.grid(column=2, row=1)
+
+# Set an initial flip timer (needed to prevent errors if `next_card` isn't called immediately
+flip_timer = window.after(3000, func=flip_card)
 
 # Start with a Random Word
 next_card()
-
 
 window.mainloop()
 
