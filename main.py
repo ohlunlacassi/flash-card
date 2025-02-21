ebc382a3b9fc3fe0a6525b38d84886738211f4c1
Phatjira Rungsakullikit <p.rungsakullikit1@hm.edu>
2025-02-21 08:52:55 +0100

    create card and buttons

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+from tkinter import *
+
+from pygame.examples.cursors import image
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+card_front = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front)
+canvas.grid(column=1, row=0, columnspan=3)
+
+wrong = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong, highlightthickness=0)
+wrong_button.grid(column=1, row=1)
+
+right = PhotoImage(file="images/right.png")
+right_button = Button(image=right, highlightthickness=0)
+right_button.grid(column=3, row=1)
+
+window.mainloop()
 
