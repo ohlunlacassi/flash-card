f33a9715214a527b2c00e84e2843083e1bb25500
Phatjira Rungsakullikit <p.rungsakullikit1@hm.edu>
2025-02-21 09:11:10 +0100

    add title and word on canvas

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from tkinter import *
 
-from pygame.examples.cursors import image
-
 BACKGROUND_COLOR = "#B1DDC6"
+FONT_NAME = "Arial"
 
 window = Tk()
 window.title("Flashy")
@@ -11,7 +10,13 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
-canvas.grid(column=1, row=0, columnspan=3)
+
+language_text = canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
+word_text = canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
+
+canvas.grid(column=1, row=0, columnspan=2)
+
+
 
 wrong = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong, highlightthickness=0)
@@ -19,7 +24,7 @@ wrong_button.grid(column=1, row=1)
 
 right = PhotoImage(file="images/right.png")
 right_button = Button(image=right, highlightthickness=0)
-right_button.grid(column=3, row=1)
+right_button.grid(column=2, row=1)
 
 window.mainloop()
 
